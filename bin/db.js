bbebf5a168939e2823b1fdc3c087b204bc27c287
dmkjava <dmk.java@gmail.com>
2020-11-29 16:10:58 +0530

    db config changed

--- bin/db.js
+++ bin/db.js
@@ -4,8 +4,7 @@ var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'root',
-    database: 'empoperations',
-    insecureAuth : true
+    database: 'empoperations'
 });
 
 connection.connect(function (err) {
